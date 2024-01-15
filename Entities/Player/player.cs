7d1a0759a0535e212e8ba9c0fe5aaaa293995e83
AnnickS <annick.stefanin@gmail.com>
2024-01-15 17:15:23 -0500

    Added wasd movement

--- Entities/Player/player.cs
+++ Entities/Player/player.cs
@@ -3,15 +3,20 @@ using System;
 
 public partial class player : CharacterBody2D
 {
+	[Export]
 	public const float Speed = 300.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		MoveCharacter(direction);
+	}
+	
+	private void MoveCharacter(Vector2 direction)
+	{
+		Vector2 velocity = Velocity;
+		
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
