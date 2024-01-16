c8b21ea943273c92a0d38f0d8af549f1a2438cc8
JasonLovrien <jasonlovrien@gmail.com>
2024-01-15 18:09:20 -0600

    fix float volume errors

--- Menus/MusicControls.cs
+++ Menus/MusicControls.cs
@@ -3,41 +3,54 @@ using System;
 
 public partial class MusicControls : GridContainer
 {
-	private float _musicLevel = 0f;
+	private int _musicVolume = 0;
 	private int _musicBusIndex = 0;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this._InitializeMusicControls();
+		this._AddVolumeAdjustHandlers();
+	}
+	
+	private void _InitializeMusicControls() {
 		this._musicBusIndex = AudioServer.GetBusIndex("MusicBus");
-		this._musicLevel = Mathf.DbToLinear(AudioServer.GetBusVolumeDb(this._musicBusIndex));
-		
+		float currentVolume = Mathf.DbToLinear(AudioServer.GetBusVolumeDb(this._musicBusIndex));
+		this._musicVolume = (int)Math.Round(100f * currentVolume, 2);
+		this._SetMusicLevel(this._musicVolume);
+	}
+	
+	private void _SetMusicLevel(int newMusicLevel) {
+			this._musicVolume = newMusicLevel;
+			float linearVolume = (float)newMusicLevel / (float)100;
+			AudioServer.SetBusVolumeDb(this._musicBusIndex, Mathf.LinearToDb(linearVolume));
+			
+			Label musicVolumeLbl = GetNode<Label>("MusicVolume");
+			musicVolumeLbl.Text = newMusicLevel.ToString() + "%";
+	}
+	
+	private void _AddVolumeAdjustHandlers() {
 		Button lowerMusicBtn = GetNode<Button>("LowerMusicBtn");
 		Button raiseMusicBtn = GetNode<Button>("RaiseMusicBtn");
 		
 		lowerMusicBtn.Pressed += () => {
-			float newMusicLevel = this._musicLevel - 0.1f;
+			int newMusicLevel = this._musicVolume - 10;
 			
-			if(newMusicLevel <= 0f) {
-				newMusicLevel = 0f;
+			if(newMusicLevel <= 0) {
+				newMusicLevel = 0;
 			}
 			
 			this._SetMusicLevel(newMusicLevel);
 		};
 		
 		raiseMusicBtn.Pressed += () => {
-			float newMusicLevel = this._musicLevel + 0.1f;
+			int newMusicLevel = this._musicVolume + 10;
 			
-			if(newMusicLevel >= 1f) {
-				newMusicLevel = 1f;
+			if(newMusicLevel >= 100) {
+				newMusicLevel = 100;
 			}
 			
 			this._SetMusicLevel(newMusicLevel);
 		};
-	}
-	
-	private void _SetMusicLevel(float newMusicLevel) {
-			this._musicLevel = newMusicLevel;
-			AudioServer.SetBusVolumeDb(this._musicBusIndex, Mathf.LinearToDb(newMusicLevel));
 	}
 }
